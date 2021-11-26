5c2cb92cf67076592e2bd0715603c0ff84278c91
Ricardo Tavares <ricardotavaresit@gmail.com>
2021-11-26 17:23:31 +0000

    feact: create type Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,37 @@
+import { useState } from 'react';
+import { Container, Area, Header } from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem'; 
+
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name:'Buy the bread', done: false},
+    {id: 2, name:'Buy the cake', done: true}
+  ]); 
   return (
-    <h1>To Do B7WEB</h1>
+    <Container>
+      <Area>
+        <Header>
+            To Do List
+        </Header>
+
+        {/* Region for add a new task */}
+
+
+
+        {/* list of tasks */}
+
+
+        {list.map( (item, index) => (
+          <ListItem key={index} item={item}/>
+        ))}
+
+
+
+
+        <h1>To Do B7WEB</h1>
+      </Area>
+    </Container>
   );
  }
 
